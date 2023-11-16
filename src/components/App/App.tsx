ba5673c488d74769329591ce89a62af0eeceb217
ValentinLugovov <valentinlugovov@gmail.com>
2023-11-16 14:47:28 +0300

    feat: make news page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,17 +3,18 @@ import Navbar from '../features/Header/Navbar';
 import SideBar from '../features/SideBar/SideBar';
 import FindAdress from '../features/Search/FindAdress';
 import { Route, Routes } from 'react-router-dom';
+import NewsPage from '../features/News/NewsPage';
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="main">
-        <SideBar />
-        <Routes>
-          <Route path="/" element={<FindAdress />} />
-          <Route path="/address" element={<FindAdress />} />
-        </Routes>
+          <SideBar />
+          <Routes>
+            <Route path="/" element={<NewsPage />} />
+            <Route path="/address" element={<FindAdress />} />
+          </Routes>
       </div>
     </>
   );
