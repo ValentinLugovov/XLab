0c0187af819eb2f2f43b1b64778316e6ba4210e2
ValentinLugovov <valentinlugovov@gmail.com>
2023-11-16 14:53:01 +0300

    style: fix header

--- src/components/features/Header/Navbar.tsx
+++ src/components/features/Header/Navbar.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import './style/navbarStyle.css';
-// import SideBar from '../SideBar/SideBar';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+
       <div className="leftContent">
         <div>
           <img src="logo.svg" alt="logo" />
@@ -13,6 +12,7 @@ const Navbar = () => {
           <span className="textLogo">Wrench CRM</span>
         </div>
       </div>
+
       <div className="rightContent">
         <div>
           <img src="person.svg" alt="logo" />
@@ -21,7 +21,7 @@ const Navbar = () => {
           <span className="textLogo">Имя Фамилия</span>
         </div>
       </div>
-      {/* <div><SideBar /></div> */}
+      
     </nav>
   );
 };
